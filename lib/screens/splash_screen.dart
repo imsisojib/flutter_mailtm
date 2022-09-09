f385d33df3eb1ced7a3600c375a7525c73dfab2f
sirajul.sojib <sirajul.sojib@leads-bd.com>
2022-09-09 14:18:27 +0600

    app logo added.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mailtm/resources/app_images.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatelessWidget{
   const SplashScreen({Key? key}) : super(key: key);
@@ -7,7 +9,9 @@ class SplashScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Splash Screen",style: Theme.of(context).textTheme.bodyText1,),),
+      body: Center(
+        child: Image.asset(AppImages.appLogo,height: 300.h,width: 300.h,fit: BoxFit.cover,),
+      ),
     );
   }
 }
